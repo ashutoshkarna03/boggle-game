302a5be4678f64dbacb2966de4d465c72fac6581
ashutoshkarna03 <ashutosh@uxcam.com>
2020-02-11 02:02:25 +0545

    first prototype of frontend with possible required components

---
+++ frontend/src/utils/randomCharaterGenerator.js
@@ -0,0 +1,18 @@
+let randomWords = require('random-words');
+// import * as randomWords from 'random-words';
+// function to generate array of random characters of given length
+function randomCharacterGenerator(length) {
+    let result           = [];
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        // result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        result.push(characters.charAt(Math.floor(Math.random() * charactersLength)))
+    }
+    return result;
+}
+
+console.log(randomWords({exactly: 5, maxLength: 5}))
+module.exports = {
+    randomCharacterGenerator: randomCharacterGenerator
+}
